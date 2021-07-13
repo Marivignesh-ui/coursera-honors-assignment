51ec1a70cc3513c02799b277bcb7c6f2c52701c6
Marivignesh-ui <smartvignesh499@gmail.com>
2021-07-13 21:51:26 +0530

    final commit

---
+++ scripts1.js
@@ -0,0 +1,12 @@
+$("#loginbutton").click(function(){
+    $("#loginModal").modal("show");
+});
+$("#buttonclose").click(function(){
+    $("#loginModal").modal("hide");
+});
+$("#signup").click(function(){
+    $("#SignupModal").modal("show");
+});
+$("#buttonclose1").click(function(){
+    $("#SignupModal").modal("hide");
+});
